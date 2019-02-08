9fe97e4b9311aa61602b9efde37cd758e63bce5d
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-02-08 14:49:47 +0100

    Edit

--- v1/app.js
+++ v1/app.js
@@ -7,8 +7,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds=[
-		{name:"Kampung Lio", image:"https://pixabay.com/get/e83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f9c57eaeeebdb0_340.jpg"},
-		{name:"Kampung Gajah", image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f9c57eaeeebdb0_340.jpg"},
+		{name:"Kampung Lio", image:"https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f9c77ba2efb6bc_340.jpg"},
+		{name:"Kampung Gajah", image:"https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b014459cf6c37ca5eeb1_340.jpg"},
 		{name:"Hutan Rote", image:"https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"}
 ];
 
