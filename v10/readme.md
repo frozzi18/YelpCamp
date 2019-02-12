f7329eb9def3dd8db2aee78bd477cd986108ac49
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-02-12 18:53:28 +0100

    Version 10
    Version 10

--- v10/readme.md
+++ v10/readme.md
@@ -13,3 +13,24 @@
 * User can only edit his/her campgrounds
 * User can only delete his/her campgrounds
 * Hide/Show edit and delete buttons
+
+## Editing comments
+* Add edit route for comments
+* Add edit button
+* Add update route
+
+Campground Edit route   :   /campgrounds/:id/edit
+Comment Edit route      :   /campgrounds/:id/comments/:comment_id/edit
+
+## Deleting Comments
+* Add Destroy route
+* Add Delete button
+
+Campground Destroy route   :   /campgrounds/:id
+Comment Destroy route      :   /campgrounds/:id/comments/:comment_id
+
+## Authorization Part 2: Comments
+* User can only edit his/her comments
+* User can only delete his/her comments
+* Hide/show edit and delete buttons
+* Refactor middleware 
--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -51,12 +51,81 @@ router.post("/", isLoggedIn, function(req,res){
 	// redirect campground show page
 });
 
+
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+	Comment.findById(req.params.comment_id, function(err, foundComment){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {campground_id : req.params.id, comment:foundComment});
+		}
+	})
+});
+
+
+// COMMENT UPDATE
+router.put("/:comment_id",checkCommentOwnership, function(req, res){
+	// res.send("YOU HIT THE UPDATE ROUTE FOR COMMENT!");
+
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/"+ req.params.id);
+		}
+	})
+
+})
+
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, function(req,res){
+	//findByIdAndRemove
+	// res.send("THIS IS THE DESTROY COMMENT ROUTE");
+
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	})
+})
+
+
 //middleware
 function isLoggedIn(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
 	}
 	res.redirect("/login");
+};
+
+function checkCommentOwnership(req,res,next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err){
+				res.redirect("back")
+			} else {
+				//does user own the comment?
+				// console.log(foundCampground.author.id); // this is mongoose object
+				// console.log(req.user._id); //this is string, don't use === but use equals()
+
+				if(foundComment.author.id.equals(req.user._id)){
+					next();
+				} else {
+					res.redirect("back");
+				}
+				
+			}
+		});
+	} else {
+		// console.log("YOU NEED TO BE LOGGED IN TO DO THAT!!!");
+		// res.send("YOU NEED TO BE LOGGED IN TO DO THAT!!!");
+
+		res.redirect("back");
+
+	}
 }
 
 module.exports = router;
